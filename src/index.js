9936018943532f336e4601523193e3b4adee6606
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-03-02 14:45:25 -0800

    import d3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import movies from './movies';
+import { select } from 'd3';
 
 const value = movies();
 document.getElementById('test').textContent = value;
+
+const div = select('div');
+
+const svg = div.append('svg')
+                .attr('width',500)
+                .attr('height',500);
+
+const cir = svg.append('circle')
+                .attr('r',50)
+                .attr('cx',500/2)
+                .attr('cy',500/2)
+
