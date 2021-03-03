309e01b676a61fffe6f1a75252cdf1b902462499
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-03-02 20:08:37 -0800

    read data from csv file in the console

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"), 
-    filename: 'main.js',     
+    path: path.resolve(__dirname), 
+    filename: './main.js',     
   },
   resolve: {
     extensions: ['.js', '*']
