3e7868ea9b849fbce03761eb3e5537b08bfce04d
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-03-06 11:36:30 -0800

    build hover effect

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { csv, select, scaleLinear, max, scaleBand, axisBottom, axisLeft } from '
 
 document.addEventListener("DOMContentLoaded",()=>{
 
-    const margin = {top: 30, right: 30, bottom: 70, left: 60};
+    const margin = {top: 30, right: 10, bottom: 70, left: 120};
     const width = 740 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
@@ -22,12 +22,14 @@ document.addEventListener("DOMContentLoaded",()=>{
         .padding(0.2);
     const xAxis = svg.append("g")
         .attr("transform", "translate(0," + height + ")")
+        .attr("class", "X-axis")
+
 
     // Initialize the Y axis
     const y = scaleLinear()
         .range([ height, 0]);
     const yAxis = svg.append("g")
-        // .attr("class", "myYaxis")
+        .attr("class", "Y-axis")
 
 
 
@@ -40,20 +42,20 @@ document.addEventListener("DOMContentLoaded",()=>{
                 d.movies = parseFloat(d.movies);
             });
 
-            x.domain(data.map(function(d) { return d.year; }))
+            x.domain(data.map(d => d.year));
             xAxis
                 .transition()
                 .duration(1000)
                 .call(axisBottom(x))
 
             // Add Y axis
-            y.domain([0, max(data, function(d) { return +d[selectedVar] }) ]);
+            y.domain([0, max(data, d => d[selectedVar] )]);
             yAxis.transition().duration(1000).call(axisLeft(y));
 
             // variable u: map data to existing bars
             const u = svg.selectAll("rect")
                 .data(data)
-
+            
             // update bars
             u
                 .enter()
@@ -61,10 +63,10 @@ document.addEventListener("DOMContentLoaded",()=>{
                 .merge(u)
                 .transition()
                 .duration(1000)
-                    .attr("x", function(d) { return x(d.year); })
-                    .attr("y", function(d) { return y(d[selectedVar]); })
+                    .attr("x", d => x(d.year))
+                    .attr("y", d => y(d[selectedVar]))
                     .attr("width", x.bandwidth())
-                    .attr("height", function(d) { return height - y(d[selectedVar]); })
+                    .attr("height", d => height - y(d[selectedVar]))
                     .attr("fill", "#DF7172")
     
 
