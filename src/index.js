45422b3249524319f88cc353d601b80c07d3f31a
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-03-05 23:28:43 -0800

    style the chart and buttons

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { csv, select, scaleLinear, max, scaleBand, axisBottom, axisLeft } from '
 document.addEventListener("DOMContentLoaded",()=>{
 
     const margin = {top: 30, right: 30, bottom: 70, left: 60};
-    const width = 700 - margin.left - margin.right;
+    const width = 740 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
