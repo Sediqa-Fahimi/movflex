e51182cb1ea13cee65c53e95b75f695a60506969
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-03-07 20:38:00 -0800

    merge changes from main

--- main.js
+++ main.js
@@ -31246,12 +31246,12 @@ __webpack_require__.r(__webpack_exports__);
 
 document.addEventListener("DOMContentLoaded",()=>{
 
-    const margin = {top: 30, right: 30, bottom: 70, left: 60};
-    const width = 460 - margin.left - margin.right;
+    const margin = {top: 30, right: 10, bottom: 70, left: 120};
+    const width = 740 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    const svg = Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])("#my_dataviz")
+    const svg = Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])(".chart")
         .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
@@ -31265,50 +31265,77 @@ document.addEventListener("DOMContentLoaded",()=>{
         .padding(0.2);
     const xAxis = svg.append("g")
         .attr("transform", "translate(0," + height + ")")
+        .attr("class", "X-axis")
+
 
     // Initialize the Y axis
     const y = Object(d3__WEBPACK_IMPORTED_MODULE_0__["scaleLinear"])()
         .range([ height, 0]);
     const yAxis = svg.append("g")
-        // .attr("class", "myYaxis")
+        .attr("class", "Y-axis")
 
-
+    const div = Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])('body')
+        .append('div')
+        .attr('class', 'info-hint')
+        .style('opacity', 0)
 
     function update(selectedVar) {
 
-        Object(d3__WEBPACK_IMPORTED_MODULE_0__["csv"])("./data/tvs_movies.csv").then((data) => {
+        Object(d3__WEBPACK_IMPORTED_MODULE_0__["csv"])("./data/categories.csv").then((data) => {
             data.forEach(d => {
                 d.year = parseFloat(d.year);
                 d.tv_shows = parseFloat(d.tv_shows);
                 d.movies = parseFloat(d.movies);
             });
 
-            x.domain(data.map(function(d) { return d.year; }))
+            x.domain(data.map(d => d.year));
             xAxis
                 .transition()
                 .duration(1000)
                 .call(Object(d3__WEBPACK_IMPORTED_MODULE_0__["axisBottom"])(x))
 
             // Add Y axis
-            y.domain([0, Object(d3__WEBPACK_IMPORTED_MODULE_0__["max"])(data, function(d) { return +d[selectedVar] }) ]);
+            y.domain([0, Object(d3__WEBPACK_IMPORTED_MODULE_0__["max"])(data, d => +d[selectedVar] )]);
             yAxis.transition().duration(1000).call(Object(d3__WEBPACK_IMPORTED_MODULE_0__["axisLeft"])(y));
+
+
 
             // variable u: map data to existing bars
             const u = svg.selectAll("rect")
                 .data(data)
-
+            
             // update bars
             u
                 .enter()
                 .append("rect")
                 .merge(u)
+                .on('mouseover', function(e,d){
+                    Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])(this).transition().duration('50')
+                    .attr('opacity', '.85');
+
+                    div.transition()
+                        .duration(50)
+                        .style('opacity', 1);
+                    console.log(e)
+                    div.html(d[selectedVar])
+                        .style('left', (e.pageX + 5) + 'px')
+                        .style('top', (e.pageY - 15) + 'px')
+                })
+                .on('mouseout', function(e,d){
+                    Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])(this).transition().duration('50')
+                    .attr('opacity', '1');
+
+                    div.transition()
+                        .duration(50)
+                        .style('opacity', 0);
+                })
                 .transition()
                 .duration(1000)
-                    .attr("x", function(d) { return x(d.year); })
-                    .attr("y", function(d) { return y(d[selectedVar]); })
+                    .attr("x", d => x(d.year))
+                    .attr("y", d => y(d[selectedVar]))
                     .attr("width", x.bandwidth())
-                    .attr("height", function(d) { return height - y(d[selectedVar]); })
-                    .attr("fill", "#F85B49")
+                    .attr("height", d => height - y(d[selectedVar]))
+                    .attr("fill", "#DF7172")
     
 
         }).catch(err => {
@@ -31316,10 +31343,14 @@ document.addEventListener("DOMContentLoaded",()=>{
         })
 
     }
-    update('movies');
+    update('tv_shows');
     Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])('#tv-shows-btn').on('click',() => update('tv_shows'));
     Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])('#movies-btn').on('click',() => update('movies'));
- 
+    Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])('#dramas-btn').on('click',() => update('dramas'));
+    Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])('#comedies-btn').on('click',() => update('comedies'));
+    Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])('#action-btn').on('click',() => update('action'));
+    Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])('#kids-tv-btn').on('click',() => update('kids_tv'));
+    
 });
 
 
